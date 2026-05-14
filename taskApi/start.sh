0ad2eb65b6874058bf921064c2dc846a7b95fc31
Devansh <agarwaldevansha007@gmail.com>
2026-05-14 17:49:30 +0530

    added start.sh extra

--- taskApi/start.sh
+++ taskApi/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+set -e
+
+echo "==> Running migrations..."
 python manage.py migrate
-celery -A core worker --loglevel=info --detach --logfile=/tmp/celery.log --pidfile=/tmp/celery.pid
-celery -A core beat --loglevel=info --detach --logfile=/tmp/celerybeat.log --pidfile=/tmp/celerybeat.pid
-tail -f /tmp/celery.log &
+
+echo "==> Starting Celery worker..."
+celery -A core worker --loglevel=info &
+
+echo "==> Starting Celery beat..."
+celery -A core beat --loglevel=info &
+
+echo "==> Starting Gunicorn..."
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 2
